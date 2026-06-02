05291ef25a158ee984afcfc7b6f47d804e860420
nadine2407 <nad.masrour@gmail.com>
2026-06-02 12:52:11 +0200

    add OpenAPI documentation

--- src/main/java/com/example/financetracker/FinanceTrackerApplication.java
+++ src/main/java/com/example/financetracker/FinanceTrackerApplication.java
@@ -1,8 +1,22 @@
 package com.example.financetracker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Personal Finance Tracker API",
+        version = "1.0.0",
+        description = "API for managing personal finances: transactions, budgets, goals and accounts.",
+        contact = @Contact(
+            name = "Nadine MASROUR & Amina YOUS",
+            email = "nad.masrour@gmail.com | amina.yous@dauphine.eu"
+        )
+    )
+)
 @SpringBootApplication
 public class FinanceTrackerApplication {
 
